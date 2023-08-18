40dffac453fa7c5f0549f1a1ddac47c5bd042d90
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2023-08-18 14:25:20 +0100

    Fix NVIM config

--- files/windows/home/AppData/Local/nvim/lua/options.lua
+++ files/windows/home/AppData/Local/nvim/lua/options.lua
@@ -1,39 +1,17 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+local opts = {
+  shiftwidth = 2,
+  tabstop = 2,
+  expandtab = true,
+  smartindent = true,
+  wrap = false,
+  termguicolors = true,
+  number = true,
+  relativenumber = true,
+  timeout = true,
+  timeoutlen = 500,
+}
+
+-- Set options from table
+for opt, val in pairs(opts) do
+  vim.o[opt] = val
 end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-require("lazy").setup({
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
-    end,
-  },
-
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        transparent_mode = true,
-      })
-    end,
-  },
-
-  "nvim-lualine/lualine.nvim",
-})
-
-vim.cmd("colorscheme gruvbox")
